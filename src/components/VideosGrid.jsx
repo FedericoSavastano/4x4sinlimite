a1fa4f91914a9437d8a69cb5d31b7585c66bb49c
FedericoSavastano <f.savastano@hotmail.com>
2025-05-03 14:11:04 -0300

    new video

--- src/components/VideosGrid.jsx
+++ src/components/VideosGrid.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 function VideosGrid() {
     const videos = [
         {
+            src: 'https://youtu.be/kQkBb-1YemU?si=zZTxY8FlxtA5Rk0-',
+        },
+        {
             src: 'https://www.youtube.com/embed/H7zJHtQM4y0?si=2DgJucSD3_tu0DAz',
         },
         {
