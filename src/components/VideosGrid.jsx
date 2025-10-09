0ca7fe4e6261db687c10d80bbe47dcad227c24d4
FedericoSavastano <f.savastano@hotmail.com>
2025-10-09 13:19:37 -0300

    pina video

--- src/components/VideosGrid.jsx
+++ src/components/VideosGrid.jsx
@@ -3,6 +3,9 @@ import React from "react";
 function VideosGrid() {
   const videos = [
     {
+      src: "https://www.youtube.com/embed/m9ON7BixoJc?si=8C93Nr1eZgX4HIBw",
+    },
+    {
       src: "https://www.youtube.com/embed/vbNfbnKqZ-M?si=Q3qTDQVXoPySLiSU",
     },
     {
